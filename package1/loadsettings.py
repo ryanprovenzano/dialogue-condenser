8074de9e28ef38e951516af64db6dce75011d711
Ryan Provenzano <ryan.provenz@gmail.com>
2020-12-31 13:29:39 -0500

    consolidated recent changes

--- package1/loadsettings.py
+++ package1/loadsettings.py
@@ -2,16 +2,18 @@ import configparser
 import re
 
 config = configparser.ConfigParser()
-config.read('settings_new.conf')
+config.read('settings.conf')
 list_of_sections = config.sections()
 #Converts a list of numeric strings into a reverse sorted list of integers.
-def convert_strings_to_ints(strings):
+def convert_strings_to_ints( strings ):
+
     temp = []
     for item in strings:
         temp.append(int(re.sub('[^0-9]','', item)))
     return sorted(temp, reverse = True)
 
-def revert_to_string(input):
+def revert_to_string( input ):
+
     for i in range(len(list_of_sections)):
         section_string = list_of_sections[i]
         section_numerics_list = []
@@ -22,9 +24,6 @@ def revert_to_string(input):
         numeric_int = int(numeric_string)
         if input == numeric_int:
             return section_string
-
-
-
 
 #List of the different gap intervals
 interval_list = convert_strings_to_ints(list_of_sections)
@@ -39,6 +38,5 @@ for test in ['default', 'DEFAULT', 'Default']:
         file_suffix = 'TRIM_'
         suffix_default = True
         break
-
 if suffix_default != True:
      file_suffix = config['DEFAULT']['FileSuffix']
